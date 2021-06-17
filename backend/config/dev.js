8bfd426100c460cbc02e544678f00e2e57462345
drorGr <dror091@gmail.com>
2021-06-17 11:45:53 +0300

    Revert "adding backend"
    This reverts commit b3db0763a3a01883ee53611341010cee9bc726fc.

--- backend/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  dbURL: 'mongodb+srv://admin:admin@cluster0.9qkqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  dbName: 'tester_db',
-}
--- backend/config/prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  dbURL: 'mongodb+srv://puki:puki123@cluster0.oaihw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  dbName: 'camar21_db',
-}
